d4d4b46b2fead912ae1bfe47c046c2aa7da421c4
nakano <nakano@skinbarrier-no-iMac.local>
2014-10-15 07:03:24 +0900

    ad and ar file

--- exome_analysis.py
+++ exome_analysis.py
@@ -5,7 +5,6 @@ def check_known_genes(known_genes, ad_file):
 	known_snps = []
 	for line in ad_file:
 		tmp  = line.split('\t')
-		print tmp[10]
 		if tmp[10] in known_genes == True:
 			print 'Found %s in VCF'%tmp[10]
 			if int(tmp[23].find('rs')) == -1:
@@ -13,6 +12,21 @@ def check_known_genes(known_genes, ad_file):
 
 
 	return known_snps
+
+def check_standard_mutation(lines):
+	res=[]
+	for line in lines:
+		tmp = line.split('\t')
+		#maf < 0.05
+		try:
+			if float(tmo[41]) < 0.05:
+				if int(tmp[23].find('rs')) == -1:
+					if int(tmp[11].find('nonsense')) == -1:
+						print line
+                                        	res.append(line)
+		except:
+			pass
+	return res
 
 if __name__ == "__main__":
 
@@ -25,31 +39,26 @@ if __name__ == "__main__":
 	f_ar = open(argvs[1])
 	text_ar = f_ar.read()
 	f_ar.close
-
         lines_ar = text_ar.split('\r')
 
         #AD_file for one patient
 	f_ad = open(argvs[2])
 	text_ad = f_ad.read()
 	f_ad.close
+        lines_ad = text_ad.split('\r')
 
         #Export file
-        list_name = argvs[1].split('/')[-1]
+	list_name = argvs[1].split('/')[-1][0:10]
 	f1 = open('build/candidate_list_%s'%list_name, 'w')
         f1.write(lines_ar[2])
         f1.write('\n')
 
-
-	for line in lines_ar:
-		tmp = line.split('\t')
-		#maf < 0.05
-		try:
-			if float(tmp[41]) < 0.05:
-				#rs ?
-				if int(tmp[23].find('rs')) == -1:
-					f1.write(line)
-		except:
-			pass
+	res_ar = check_standard_mutation(lines_ar)
+	res_ad = check_standard_mutation(lines_ad)
+	for ar in res_ar:
+		f1.write(ar)
+	for ad in res_ad:
+		f1.write(ad)
 
 	#Known Genes
 	f_known=open(argvs[3])
@@ -57,8 +66,9 @@ if __name__ == "__main__":
         f_known.close()
 
 	known_genes=text_known.strip('\n').split(',')
-	print known_genes
-	known_snps = check_known_genes(known_genes, lines_ad)
+	print "Known Related Genes %s" % known_genes
+	known_snps = check_known_genes(known_genes, lines_ar)
+
         f1.write('known genes \n')
 	for k_snp in known_snps:
         	f1.write(k_snp)
